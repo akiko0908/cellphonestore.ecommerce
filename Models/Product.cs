f9bd7c3a63a4bfddc52759a8ad6c6bda4c64a7d7
Hau Nguyen <huutho1999@gmail.com>
2020-11-12 12:57:11 +0700

    Upload properties Product.cs

--- Models/Product.cs
+++ Models/Product.cs
@@ -15,10 +15,26 @@ namespace CellphoneStoreEcommerce.Models
     {
         [Key]
         public string product_ID { get; set; }
+
+        [Required(ErrorMessage = "Nhập thông tin cho trường này!!!")]
+        [Display(Name = "Tên sản phẩm")]
         public string product_Name { get; set; }
+
+        [Required(ErrorMessage = "Nhập thông tin cho trường này!!!")]
+        [Display(Name = "Giá sản phẩm")]
+        [Range(10000, 100000000, ErrorMessage = "Giá thấp nhất là 10,000vnđ và tối đa là 100,000,000vnđ")]
         public double product_Price { get; set; }
+
+        [Required(ErrorMessage = "Nhập thông tin cho trường này!!!")]
+        [Display(Name = "Số lượng sản phẩm")]
+        [Range(1, 1000, ErrorMessage = "Số lượng tối thiểu là 1 và tối đa là 1,000")]
         public int product_Quantity { get; set; }
+
+        [Display(Name = "Hệ điều hành sản phẩm")]
         public string product_OS { get; set; }
+
+        [Display(Name = "Mô tả sản phẩm")]
+        public string product_Description { get; set; }
 
         public Product() { }
 
