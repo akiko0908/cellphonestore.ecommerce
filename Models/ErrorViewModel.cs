67950a806355e9f3e739d49edae9dfb89e82ac1f
Hau Nguyen <huutho1999@gmail.com>
2020-11-12 08:36:29 +0700

    Create new project Ecommerce CellphoneStore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CellphoneStoreEcommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
