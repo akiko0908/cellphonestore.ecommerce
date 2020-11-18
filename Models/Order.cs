b5a1183c0e3b6d47434bcfb297e60e16bf2d5f35
Hau Nguyen <huutho1999@gmail.com>
2020-11-18 11:35:51 +0700

    Add identity into project

--- Models/Order.cs
+++ Models/Order.cs
@@ -17,6 +17,24 @@ namespace CellphoneStoreEcommerce.Models
         [Display(Name = "Mã hóa đơn")]
         public int order_ID { get; set; }
 
+        [Display(Name = "Ngày đặt hàng")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime order_CreateOnDay { get; set; }
 
+        [Display(Name = "Tình trạng đơn hàng")]
+        public int order_Status { get; set; }
+
+        [Display(Name = "Ngày cập nhập")]
+        public DateTime order_UpdateOnDay { get; set; }
+
+        [Display(Name = "Tổng tiền đơn hàng")]
+        public decimal order_Total { get; set; }
+
+        [Display(Name = "Phương thức thanh toán")]
+        public string order_PaymentMethod { get; set; }
+
+        [Display(Name = "Tiền chiết khấu")]
+        public decimal order_AmountDiscount { get; set; }
     }
 }
