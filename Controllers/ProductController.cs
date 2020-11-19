e2f0c9d5168aaa63e1e1016bfbe0d1090e128bc1
Hau Nguyen <huutho1999@gmail.com>
2020-11-19 23:01:18 +0700

    Re-upload project modified func and method

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -6,12 +6,32 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CellphoneStoreEcommerce.Models;
+using CellphoneStoreEcommerce.Data;
 
 namespace CellphoneStoreEcommerce.Controllers
 {
     public class ProductController : Controller
     {
+        public ProductController() { }
+
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product newProduct)
+        {
+            return View();
+        }
+
+        public IActionResult Details(int? i)
         {
             return View();
         }
